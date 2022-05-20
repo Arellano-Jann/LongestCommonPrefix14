4d67919926c0c42aba627818f6a3b1e751e43706
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-05-20 13:48:18 -0700

    passes test case 4

--- solution.py
+++ solution.py
@@ -2,13 +2,20 @@ class Solution(object):
     def longestCommonPrefix(self, strs):
         common = ""
         strs.sort(key = lambda x: len(x))
-        for word1 in strs[-1::1]:
+        print(strs)
+        if len(strs) == 1:
+            return "".join(strs)
+        for word1 in strs[1:]:
+            print(word1, "word1")
             word2 = strs[0]
             for char in range(len(word2)):
                 if word1[char] == word2[char]:
                     common += word1[char]
+                else:
+                    break
             common += " "
         commonList = common.split()
+        print(commonList)
         if len(commonList) > 1:
             return self.longestCommonPrefix(commonList)
         return "".join(commonList)
@@ -21,5 +28,5 @@ class Solution(object):
         
 print(Solution().longestCommonPrefix(["flower","flow","flight"]))
 print(Solution().longestCommonPrefix(["dog","racecar","car"]))
-print(Solution().longestCommonPrefix(["c","c"]))
+print(Solution().longestCommonPrefix(["cir","car"]))
 print(Solution().longestCommonPrefix(["a"]))
