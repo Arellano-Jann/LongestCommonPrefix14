dd40dbca864d7e0a3f9a7db8746fbdc1332811eb
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-05-20 14:46:12 -0700

    works

--- solution.py
+++ solution.py
@@ -1,24 +1,14 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
         strs.sort(key = lambda x: len(x))
-        print(strs)
         common = ""
-        for charIndex in range(1, len(strs[0])):
-            for word in strs[1:]:
-                if word.startswith(strs[0][0:charIndex]):
-                    print(word, "startswith", strs[0][0:charIndex])
-                    common += strs[0][charIndex-1]
-                else:
-                    break
-        if len(strs) == 1:
-            return "".join(strs)
-        common = "".join(dict.fromkeys(common))
-        for x in strs:
-            if x.startswith(common):
-                continue
-            else:
-                return ""
-                
+        for index, char in enumerate(strs[0]):
+            for word in strs:
+                if word[index] != char:
+                    return common
+            common += char
+        return common
+
                 
         """
         :type strs: List[str]
